1b2a241ae8f538b38bf61dfade4070b4dc2e62cf
Willem Steyn <leftknee4@gmail.com>
2023-03-04 06:59:34 +0200

    Initial commit to get code in

---
+++ MinimalAPI/MinimalApi/MinimalApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinimalApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
